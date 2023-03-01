85c7df568bfb82943c6570d8a791bc1fcaa51ac7
jeffos1 <villa4ks@gmail.com>
2023-03-01 00:01:55 -0500

    changes

--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -7,14 +7,15 @@ const noticationReducer = createSlice({
         }
     }
 })
-
+//action creator
 export const setNotification = (message, time) => dispatch => {
+    //takes a message and a duration in seconds, and returns a thunk that dispatches the setNotification action with the message and sets a timeout to clear the notification after the specified duration.
     dispatch(addNotification(message))
 
     setTimeout(() => {
         dispatch(addNotification(''))
     }, 1000 * time);
 }
-
+//-action creator-----------------------
 export const {addNotification} = noticationReducer.actions
 export default noticationReducer.reducer
